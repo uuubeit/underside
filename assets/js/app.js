2865fa5f68cb07a3b02044831e04dddf3e8901f4
Igor <termsubuntu@gmail.com>
2026-03-24 02:39:36 +0300

    sdfл

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,56 @@
+let observer;
 
+function initObserver() {
+  const header = document.getElementById('header');
+  const menu_icon = document.getElementById('menu-icon-mobile');
+  const cart_icon = document.getElementById('cart-icon-mobile');
+  const headerHeight=header.offsetHeight;
+  const block = document.getElementsByClassName('preview')[0];
 
+  const observer = new IntersectionObserver(
+    (entries) => {
+      const entry = entries[0];
 
+      if (!entry.isIntersecting) {
+        // header.classList.add('scrolled');
+        header.classList.add('active');
+        menu_icon.setAttribute('src','/assets/icons/menu-icon-reverse.svg');
+        cart_icon.setAttribute('src','/assets/icons/cart-icon-reverse.svg');
+      } else {
+        // header.classList.remove('scrolled');
+        header.classList.remove('active');
+        menu_icon.setAttribute('src','/assets/icons/menu-icon.svg');
+        cart_icon.setAttribute('src','/assets/icons/cart-icon.svg');
+
+      }
+    },
+    {
+  rootMargin: `-${headerHeight}px 0px 0px 0px`
+
+    }
+  );
+
+  observer.observe(block);
+
+}
+
+function destroyObserver() {
+  if (observer) observer.disconnect();
+}
+
+const media = window.matchMedia("(max-width: 768px)");
+
+if (media.matches) {
+  initObserver();
+}
+
+media.addEventListener("change", (e) => {
+  if (e.matches) {
+    initObserver();   // включили
+  } else {
+    destroyObserver(); // выключили
+  }
+});
 
 
 const track = document.getElementById('track');
@@ -12,23 +62,23 @@ let touchStartX = 0;
 let touchEndX = 0;
 
 track.addEventListener('touchstart', e => {
-    touchStartX = e.changedTouches[0].screenX;
+  touchStartX = e.changedTouches[0].screenX;
 });
 
 track.addEventListener('touchend', e => {
-    touchEndX = e.changedTouches[0].screenX;
-    handleSwipe();
+  touchEndX = e.changedTouches[0].screenX;
+  handleSwipe();
 });
 
 function handleSwipe() {
-    if (touchStartX - touchEndX > 50) {
-        // Свайп влево -> следующий слайд
-        btnRight.click();
-    }
-    if (touchEndX - touchStartX > 50) {
-        // Свайп вправо -> предыдущий слайд
-        btnLeft.click();
-    }
+  if (touchStartX - touchEndX > 50) {
+    // Свайп влево -> следующий слайд
+    btnRight.click();
+  }
+  if (touchEndX - touchStartX > 50) {
+    // Свайп вправо -> предыдущий слайд
+    btnLeft.click();
+  }
 }
 
 // 1. Клонируем элементы
@@ -90,16 +140,19 @@ track.addEventListener('transitionend', () => {
 });
 
 const toggleInput = document.getElementById('mode-toggle');
+const buyButton = document.getElementById('catalog-buy-button');
 const catalog = document.querySelector('.catalog');
 
 toggleInput.addEventListener('change', () => {
-    if (toggleInput.checked) {
-        // Если выбрали "Изнанка"
-        catalog.classList.add('active-back');
-    } else {
-        // Если выбрали "Внешка"
-        catalog.classList.remove('active-back');
-    }
+  if (toggleInput.checked) {
+    // Если выбрали "Изнанка"
+    catalog.classList.add('active-back');
+    buyButton.classList.add('active-back');
+  } else {
+    // Если выбрали "Внешка"
+    catalog.classList.remove('active-back');
+    buyButton.classList.remove('active-back');
+  }
 });
 
 
@@ -113,26 +166,26 @@ let currentIndex = 0;
 const slidesCount = 3;
 
 function updateSlider() {
-    // Сдвигаем ленту
-    skate_track.style.transform = `translateX(-${currentIndex * 100}%)`;
-    
-    // Блокируем кнопки на границах
-    prevBtn.disabled = currentIndex === 0;
-    nextBtn.disabled = currentIndex === slidesCount - 1;
+  // Сдвигаем ленту
+  skate_track.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+  // Блокируем кнопки на границах
+  prevBtn.disabled = currentIndex === 0;
+  nextBtn.disabled = currentIndex === slidesCount - 1;
 }
 
 nextBtn.addEventListener('click', () => {
-    if (currentIndex < slidesCount - 1) {
-        currentIndex++;
-        updateSlider();
-    }
+  if (currentIndex < slidesCount - 1) {
+    currentIndex++;
+    updateSlider();
+  }
 });
 
 prevBtn.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        updateSlider();
-    }
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlider();
+  }
 });
 
 // Инициализация кнопок при загрузке
@@ -141,21 +194,21 @@ updateSlider();
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const faqItems = document.querySelectorAll('.faq-item');
+  const faqItems = document.querySelectorAll('.faq-item');
 
-    faqItems.forEach(item => {
-        const question = item.querySelector('.faq-question');
-        
-        question.addEventListener('click', () => {
-            const isActive = item.classList.contains('active');
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
 
-            // Закрываем все открытые (если хочешь, чтобы одновременно был открыт только один)
-            faqItems.forEach(el => el.classList.remove('active'));
+    question.addEventListener('click', () => {
+      const isActive = item.classList.contains('active');
 
-            // Если кликнули по закрытому — открываем
-            if (!isActive) {
-                item.classList.add('active');
-            }
-        });
+      // Закрываем все открытые (если хочешь, чтобы одновременно был открыт только один)
+      faqItems.forEach(el => el.classList.remove('active'));
+
+      // Если кликнули по закрытому — открываем
+      if (!isActive) {
+        item.classList.add('active');
+      }
     });
+  });
 });
